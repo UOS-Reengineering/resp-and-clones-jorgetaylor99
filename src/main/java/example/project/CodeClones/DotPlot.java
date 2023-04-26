691d5bd31575b98f446b76bc1668ed60d3a7d785
jorge <jorgetaylor@protonmail.com>
2023-04-26 14:24:09 +0100

    part 2: managing code clones completed

--- src/main/java/example/project/CodeClones/DotPlot.java
+++ src/main/java/example/project/CodeClones/DotPlot.java
@@ -3,7 +3,23 @@ package example.project.CodeClones;
 public class DotPlot {
 
     public static void printDotPlot(char[] sequence1, char[] sequence2) {
-        // TODO: complete this function to print the dot plot of two string arrays
-    }
 
+        System.out.print("\t");
+        for (char c : sequence2) {
+            System.out.print(c + "\t");
+        }
+        System.out.println();
+
+        for (char rowChar : sequence1) {
+            System.out.print(rowChar + "\t");
+            for (char colChar : sequence2) {
+                if (rowChar == colChar) {
+                    System.out.print("+\t");
+                } else {
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- src/main/java/example/project/CodeClones/JaccardSimilarity.java
+++ src/main/java/example/project/CodeClones/JaccardSimilarity.java
@@ -6,8 +6,16 @@ import java.util.Set;
 public class JaccardSimilarity {
 
     public static double computeJaccardSimilarity(Set<String> set1, Set<String> set2) {
-        // TODO: implement this function to compute the Jaccard similarity between two string arrays
-        return -1.0;
+        Set<String> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        Set<String> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        if (union.isEmpty()) {
+            return 0.0;
+        }
+        return (double) intersection.size() / union.size();
     }
 
 }
